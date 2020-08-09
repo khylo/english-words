5fce3de5fed59567c65a971fdfedd503340f91a8
keithhyland <keithhyland@penguin>
2020-08-09 15:48:23 +0100

    adding real world check and numAnagrams

--- src/main/java/com/khylo/Anagram.java
+++ src/main/java/com/khylo/Anagram.java
@@ -20,7 +20,6 @@ import java.util.Collection;
 
 public class Anagram{
     Collection<String> realWordSet;
-    boolean realWordsOnly;
     /* code for streaming characters.. Not code-points important
     String s = "abc-de3-2fg";
 String s1 = s.chars().filter(Character::isLetter)
@@ -31,12 +30,6 @@ System.out.println(s1);
 
     Anagram() throws IOException{
         loadRealWords();
-        realWordsOnly=true;
-    }
-    Anagram(boolean loadRealWords) throws IOException{
-        if(loadRealWords)
-            loadRealWords();
-        realWordsOnly=loadRealWords;
     }
 
     private void loadRealWords() throws IOException{
@@ -53,19 +46,21 @@ System.out.println(s1);
         System.out.println(s);
     }
 
-    public Set<String> getAnagrams(String s, boolean realWordsOnly){
-        this.realWordSet=realWordSet;
+    public Set<String> getAnagrams(String s, int subAnagramsMin){
+        return getAnagrams(s, subAnagramsMin, true);
+    }
+    public Set<String> getAnagrams(String s, int subAnagramsMin,boolean realWordsOnly){
         Set<String> ret = new HashSet<>();
         //Set<String> charSet = new HashSet<>();charSet.add("a");charSet.add("b");charSet.add("c");
         //Set<String> charSet = Set.of(s.split(".")); // java 9+ see https://stackoverflow.com/questions/3064423/how-to-convert-an-array-to-a-set-in-java
         Collection<String> charSet = new ArrayList<>(Arrays.asList(s.split("")));
         //Set<String> charSet = s.chars().map(cp ->new StringBuilder().appendCodePoint(cp).toString()).collect( Collectors.toCollection(HashSet::new));
- 
-        anagram("", charSet, ret);
+        log("Total anagrams = "+numAnagrams(s));
+        anagram("", charSet, ret,subAnagramsMin, realWordsOnly);
         return ret;
     }
 
-    private void anagram(String sanagram, Collection<String> stringSet, Set<String> ret){
+    private void anagram(String sanagram, Collection<String> stringSet, Set<String> ret, int subAnagramsMin, boolean realWordsOnly){
         if(stringSet.size()==0){
             if(realWordsOnly){
                 if(!realWordSet.contains(sanagram))
@@ -73,11 +68,15 @@ System.out.println(s1);
             }
             ret.add( sanagram);
         }
+        if(sanagram.length()>=subAnagramsMin){
+            if(realWordSet.contains(sanagram))
+                ret.add(sanagram);
+        }
         //stringSet.stream().forEach(s -> anagram(sanagram+s,
         stringSet.stream().forEach(s -> {
             List subset = new ArrayList(stringSet);
             subset.remove(s); 
-            anagram(sanagram+s, subset, ret);  
+            anagram(sanagram+s, subset, ret, subAnagramsMin, realWordsOnly);  
         });
             
         // originally had this, but if there were repeating letters it would remove them both, added  findFirst but that wont work. Need some global counter
@@ -87,13 +86,25 @@ System.out.println(s1);
          return ;
     }
 
+    public static int numAnagrams(String s){
+        Set<String> set = new HashSet<>(Arrays.asList(s.split("")));
+        return factorial(s.length());
+    }
+
+    public static int factorial(int i){
+        if(i==1)
+            return 1;
+        return i*factorial(i-1);
+    }
+
     
 
     public static void main(String args[])throws IOException{
         //Set<String> set = getAnagrams("soartrip");
         Anagram anagram = new Anagram();
         //Set<String> set = anagram.getAnagrams("abb", true);
-        Set<String> set = anagram.getAnagrams("soartrip", true);
+        //Set<String> set = anagram.getAnagrams("soartrip", 4, true);
+        Set<String> set = anagram.getAnagrams("moneyl", 4, true);    
         Anagram.log(set.size()+" Results");
         Anagram.log(set);
         //Anagram.log(getAnagrams("soartrip"));
